98758b130ee1e2de454f635af5e7e5f882a404b2
Melanie Sumner <melanie@hashicorp.com>
2021-11-30 11:27:12 -0600

    content and other tweaks

--- app/components/we-quote.js
+++ app/components/we-quote.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 
 export default class WeQuoteComponent extends Component {
   get cite() {
-    return this.args.cite ?? null;
+    return this.args.cite ?? 'An Ember Community Member';
   }
   get citeUrl() {
     return this.args.citeUrl ?? null;
