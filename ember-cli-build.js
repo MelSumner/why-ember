710f157c14f46faf8c1687edca24e3529437caeb
Melanie Sumner <melanie@hashicorp.com>
2021-12-04 15:17:17 -0600

    added highlightjs option

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    'ember-cli-markdown-templates': {
+      syntaxHighlight: true,
+    },
   });
-
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
@@ -19,6 +21,7 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('node_modules/highlightjs/styles/default.css');
 
   return app.toTree();
 };
