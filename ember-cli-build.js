50572511a68444af217e75b2f3bbb4c26bdf2bac
Melanie Sumner <melanie@hashicorp.com>
2021-12-07 19:29:32 -0600

    Added content to ember for react devs page

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,13 +5,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
-    'ember-cli-markdown-templates': {
-      syntaxHighlight: true,
+    'ember-prism': {
+      theme: 'twilight',
+      components: ['markup', 'handlebars', 'javascript'],
+      plugins: ['toolbar', 'copy-to-clipboard'],
     },
   });
 
-  // Import highlight styles
-  app.import('node_modules/highlightjs/styles/a11y-dark.css');
-
+  app.import('app/styles/prism-dracula.css');
   return app.toTree();
 };
